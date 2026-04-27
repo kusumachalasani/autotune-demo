60f15a71f5712c51ce7227152b04c9e56be1bcea
kusumachalasani <kchalasa@redhat.com>
2026-04-27 12:56:20 +0530

    Re-check for hpo server

--- hpo/hpo_demo_setup.sh
+++ hpo/hpo_demo_setup.sh
@@ -127,6 +127,10 @@ function hpo_install() {
                         SERVICE_STATUS_NATIVE=$(ps -ef | grep service.py | grep -v grep | awk '{print $2}')
                         echo "Before SERVICE_STATUS_NATIVE= ${SERVICE_STATUS_NATIVE}"
                         ps -ef | grep src/service.py | grep -v grep | awk '{print $2}' | xargs kill -9 >/dev/null 2>&1
+						
+                        SERVICE_STATUS_NATIVE=$(ps -ef | grep service.py | grep -v grep | awk '{print $2}')
+                        echo "Before SERVICE_STATUS_NATIVE= ${SERVICE_STATUS_NATIVE}"
+                        ps -ef | grep src/service.py | grep -v grep | awk '{print $2}' | xargs kill -9 >/dev/null 2>&1
 
 			echo "Starting hpo with  ./deploy_hpo.sh -c ${CLUSTER_TYPE} -p 8092 --rest"
 			echo
