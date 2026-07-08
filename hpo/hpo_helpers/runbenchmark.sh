7154e55f5b6998fdadc35ddba923b12260753635
kusumachalasani <kchalasani@ibm.com>
2026-07-08 14:16:23 +0530

    Update jenkins token

--- hpo/hpo_helpers/runbenchmark.sh
+++ hpo/hpo_helpers/runbenchmark.sh
@@ -111,7 +111,7 @@ if [[ ${BENCHMARK_RUN_THRU} == "jenkins" ]]; then
 	      query=${query%&}
 	      jobUrl="https://${JENKINS_MACHINE_NAME}:${JENKINS_EXPOSED_PORT}/job/${JENKINS_SETUP_JOB}/buildWithParameters?$query"
         else
-	      JENKINS_SETUP_TOKEN=2018
+	      JENKINS_SETUP_TOKEN="web"
 	      declare -A params
 	      params=(
 		      ["token"]="${JENKINS_SETUP_TOKEN}"
